39d5d6de5319f3749ee241e85c3201607ba0c6c5
EngMjari <mohammad.jarii@gmail.com>
2023-12-22 18:12:43 +0330

    Create Password View With React useState and fix Readme File

--- src/Components/Password.js
+++ src/Components/Password.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import styled from 'styled-components';
+import { Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+const Wrapper = styled(Container)`
+    text-align : center!important;
+    padding : 30px 0;
+`
 
+const PassView = styled(FontAwesomeIcon)`
+cursor: pointer;
+&:hover {
+    
+}
+`
 const Password = () => {
-    return (
-        <div>
+    const [password, setPassword] = useState("password")
 
-        </div>
+    return (
+        <Wrapper>
+            <InputGroup>
+                <InputGroup.Text id="PasswordInput">Password</InputGroup.Text>
+                <Form.Control type={password} id='PasswordInput' />
+                <InputGroup.Text>
+                    <PassView onMouseDown={() => setPassword("text")} onMouseUp={() => setPassword('password')} onMouseLeave={() => setPassword('password')} icon={faEye} />
+                </InputGroup.Text>
+
+            </InputGroup>
+            <Form.Text id="passwordHelpBlock" muted>
+                Your password must be 8-20 characters long, contain letters and numbers,
+                and must not contain spaces, special characters, or emoji.
+            </Form.Text>
+        </Wrapper>
     );
 }
 
